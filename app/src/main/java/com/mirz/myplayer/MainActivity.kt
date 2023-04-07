9389ce817d133f6447515b85945f1c77e29118dd
Mirza Ukasyah <mirzauksyh@gmail.com>
2023-04-07 14:16:30 +0700

    Bind ExoPlayer with Activity Lifecycle

--- app/src/main/java/com/mirz/myplayer/MainActivity.kt
+++ app/src/main/java/com/mirz/myplayer/MainActivity.kt
@@ -4,25 +4,69 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.Util
 import com.mirz.myplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
-        const val URL_AUDIO = "https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3"
+        const val URL_VIDEO_DICODING =
+            "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+        const val URL_AUDIO =
+            "https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3"
     }
+
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-
-        val player = ExoPlayer.Builder(this).build()
         viewBinding.videoView.player = player
+    }
 
-        val mediaItem = MediaItem.fromUri(URL_AUDIO)
-        player.setMediaItem(mediaItem)
-        player.prepare()
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23)
+            initializePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT <= 23 || player == null)
+            initializePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23)
+            releasePlayer()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.release()
+        player = null
+    }
+
+
+    private fun initializePlayer() {
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        val anotherMediaItem = MediaItem.fromUri(URL_AUDIO)
+
+        player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            viewBinding.videoView.player = exoPlayer
+            exoPlayer.setMediaItem(mediaItem)
+            exoPlayer.addMediaItem(anotherMediaItem)
+            exoPlayer.prepare()
+        }
     }
 }
